d278fc82aeef3b5a4e6f5e39d7d7a089927e53a7
David <David@Blackledge.com>
2021-11-07 11:14:11 -0700

    Add project files.

---
+++ PiApp/MainWindowViewModel.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace PiApp
+{
+
+    internal class MainWindowViewModel
+    {
+        public FlowDocument Document
+        {
+            get; set;
+        }
+        //public RichTextBox RichTextBox { get; private set; }
+        public ICommand LoadPoemCommand { get; } = new RelayCommand<MainWindowViewModel>(
+            (MainWindowViewModel model) => ProcessTextFile(new OpenFileDialog(), (filename) => model.LoadPoemFromFile(filename))
+            );
+
+        public ICommand SavePoemCommand { get; } = new RelayCommand<MainWindowViewModel>(
+            (MainWindowViewModel model) => ProcessTextFile(new SaveFileDialog(), (filename) => model.SavePoemToFile(filename))
+            );
+
+        public ICommand ClearPoemCommand { get; } = new RelayCommand<MainWindowViewModel>(
+            (MainWindowViewModel model) => model.Clear()
+            );
+
+        private RunDigit title;
+
+        public MainWindowViewModel()
+        {
+            Document = new FlowDocument();
+            //RichTextBox = new RichTextBox(Document);
+            //RichTextBox.SpellCheck.IsEnabled = true;
+
+            BuildDocument();
+        }
+        public void BuildDocument() { 
+            //            3.
+            CreateTitle(3);
+
+            CreateParagraphsForDigits(
+            "1415926535" + "8979323846" + "2643383279" + "50" +
+            "2884197169" + "39937510" +
+            "5820" +
+            "9749445923" + "0" +
+            "781640" +
+            "628620" +
+            "89986280" +
+            "3482534211" + "70" +
+            "679"/*100 digits*/ + "821480" +
+            "8651328230" +
+            "66470" + // 121 (a logical end)
+            "9384460" +
+            "9550" +
+            "5822317253" + "5940" +
+            "8128481117" + "450" +
+            "28410" +
+            "270" +
+            "193852110" +
+            "5559644622" + "948954930" +
+            "38196"/*200 digits*/ + "4428810" + // 207 (a logical end - next sentence is 37 words long)
+
+            "975" + // 210 digits
+            "6659334461284756482337867831652712019091456485669234603486104543266482" + // 280
+            "1339360726024914127372458700660631558817488152092096282925409171536436" + // 350
+            "7892590360011330530548820466521384146951941511609433057270365759591953" + // 420
+            "0921861173819326117931051185480744623799627495673518857527248912279381" + // 490
+            "8301194912983367336244065664308602139494639522473719070217986094370277" + // 560
+            "0539217176293176752384674818467669405132000568127145263560827785771342" + // 630
+            "7577896091736371787214684409012249534301465495853710507922796892589235" + // 700
+            "4201995611212902196086403441815981362977477130996051870721134999999837" + // 770
+            "2978049951059731732816096318595024459455346908302642522308253344685035" + // 840
+            "2619311881710100031378387528865875332083814206171776691473035982534904" + // 910
+            "2875546873115956286388235378759375195778185778053217122680661300192787" + // 980
+            "661119590921642" + // stop at 996 digits
+            // my code splits on 0's and assumes a 0 at end of every string in split, so can't end on a 0 because split creates empty string and code acts like there were two 0's at the end.
+            //"01989" + // 1000 digits
+            "");
+
+            LoadPoemFromString(new StringBuilder()
+                .AppendLine("Pie")
+                .AppendLine()
+                .AppendLine("I have a treat " +
+                "comprised of tastes salty and sweet, " +
+                "lovingly completed, " +
+                "causing droolings for my lip, " +
+                "diameter near excess so eating adds the hip " +
+                "diameter and so filling reaffirms peace.")
+                .AppendLine("No rational desserts (like a chocolate) provide a demand emulating the sugarplum pleasures now begging “shall I?”")
+                .AppendLine("Baked infinity, no!")
+                .AppendLine("Universes without pie’s immanence must know every existence to end.")
+                .AppendLine("Dessert, treasure a rotten life.")
+                .ToString());
+
+        }
+
+        private void CreateTitle(int length)
+        {
+            title = new RunDigit(length);
+
+            Paragraph titleParagraph = new Paragraph(title);
+            titleParagraph.FontSize = 25;
+            titleParagraph.TextAlignment = System.Windows.TextAlignment.Center;
+            Document.Blocks.Add(titleParagraph);
+
+            Paragraph separator = new Paragraph(new Run("___________"));
+            separator.TextAlignment = System.Windows.TextAlignment.Center;
+            Document.Blocks.Add(separator);
+        }
+
+        private void CreateParagraphsForDigits(string v)
+        {
+            string[] paragraphs = v.Split(RunDigit.StopDigitLengthChar);
+            foreach (string p in paragraphs)
+                CreateParagraphForDigits(p);
+        }
+
+        private void CreateParagraphForDigits(string p)
+        {
+            Paragraph main = new Paragraph();
+            //main.FontFamily = new System.Windows.Media.FontFamily("Fixed");
+            foreach (char c in p)
+                CreateRunDigitForParagraph(main, int.Parse(c.ToString()));
+            CreateRunDigitForParagraph(main, RunDigit.StopDigitLength);
+            Document.Blocks.Add(main);
+        }
+
+        private void CreateRunDigitForParagraph(Paragraph main, int digit)
+        {
+            RunDigit run = new RunDigit(digit);
+            main.Inlines.Add(run);
+        }
+
+
+        private void LoadPoemFromString(string poemString)
+        {
+            int i = 0;
+
+            string[] lines = StringToPoemLoadFormat(poemString);
+            if (lines.Length < 3)
+                return;
+
+            title.SetWord(lines[0].Trim());
+
+            foreach (string line in lines.Skip(2))
+                if (string.IsNullOrEmpty(line?.Trim()))
+                    i++;
+                else
+                    LoadParagraph(i++, line.Trim());
+        }
+
+        private static readonly Regex EmptyLineRegex = new Regex(@"^\s*(\d+\s*)+$", RegexOptions.Multiline);
+        private static readonly Regex SplitStopRegex = new Regex(@"(\S)([\.\!\?])");
+        private static readonly string SplitStopReplacement = "$1 $2";
+        private static string[] StringToPoemLoadFormat(string poem)
+        {
+            return SplitStopRegex.Replace(
+                EmptyLineRegex.Replace(poem, string.Empty),
+                SplitStopReplacement)
+                .Split('\n');
+        }
+
+        private static void ProcessTextFile(FileDialog filenameRequest, Action<string> processFilename)
+        {
+            filenameRequest.Filter = "Text File|*.txt";
+            bool? result = filenameRequest.ShowDialog();
+            if (result.HasValue && result.Value)
+                processFilename.Invoke(filenameRequest.FileName);
+        }
+
+        private void LoadPoemFromFile(string filename)
+        {
+            string poem = File.ReadAllText(filename);
+            LoadPoemFromString(poem);
+        }
+
+        private void SavePoemToFile(string filename)
+        {
+            string poem = PoemToString();
+            File.WriteAllText(filename, poem);
+        }
+
+        private void Clear()
+        {
+            foreach (Paragraph p in Document.Blocks.OfType<Paragraph>().ToList())
+                foreach (Inline r in p.Inlines.ToList())
+                    SetWord(r, string.Empty);
+        }
+
+        private string PoemToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Paragraph p in Document.Blocks.OfType<Paragraph>())
+                ParagraphToString(sb, p);
+
+            return sb.ToString();
+        }
+
+        private static void ParagraphToString(StringBuilder sb, Paragraph p)
+        {
+            IEnumerable<RunDigit> words = p.Inlines.OfType<RunDigit>();
+
+            if (words.Any((x) => !string.IsNullOrEmpty(x.Word)))
+                foreach (Run r in words)
+                    sb.Append(r.Text);
+
+            sb.AppendLine();
+        }
+
+        /// <summary>
+        /// Space-delimited words to load into the i'th non-title paragraph object
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="sentence"></param>
+        private void LoadParagraph(int i, string sentence)
+        {
+            Paragraph p = (Paragraph)Document.Blocks.ElementAt(2 + i);
+            string[] words = sentence.Split(' ');
+            for (int w = 0; w < words.Length; ++w)
+                if (p.Inlines.Count > w)
+                    SetWord(p.Inlines.ElementAt(w), words[w]);
+        }
+
+        private void SetWord(Inline inline, string word)
+        {
+            (inline as RunDigit)?.SetWord(word);
+        }
+    }
+}
---
+++ PiApp/RunDigit.cs
@@ -0,0 +1,204 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace PiApp
+{
+    /// <summary>
+    /// Focusable Run that represents a digit this run must conform with
+    /// </summary>
+    internal class RunDigit : Run
+    {
+        private static readonly Regex LettersRegex = new Regex(@"[a-zA-Z]");
+        private static readonly Regex StopsRegex = new Regex(@"[\.\?\!]");
+        internal static readonly int StopDigitLength = 0;
+        internal static readonly char StopDigitLengthChar = '0';
+
+        public int Length { get; }
+
+        public RunDigit(int length)
+        {
+            this.Length = length;
+
+            Focusable = true;
+            ToolTip = string.Format("{1}", 
+                Word, 
+                Length);
+
+            SetWord(string.Empty);
+
+            MouseDown += (o, e) => {
+                this.Focus();  
+                e.Handled = true;
+            };
+            GotFocus += (o, e) => {
+                SetBackground();
+                e.Handled = true;
+            };
+            LostFocus += (o, e) =>
+            {
+                SetBackground();
+                e.Handled = true;
+            };
+            KeyDown += KeyHandler;
+            PreviewTextInput += TextHandler;
+        }
+
+        private void SetBackground()
+        {
+            if (IsFocused)
+                this.Background = System.Windows.Media.Brushes.CornflowerBlue;
+            else if (Length == StopDigitLength)
+                this.Background = System.Windows.Media.Brushes.AliceBlue;
+            else
+                this.Background = System.Windows.Media.Brushes.Transparent;
+        }
+
+        private void TextHandler(object sender, TextCompositionEventArgs e)
+        {
+            if (IsFocused)
+            {
+                SetWord(Word + e.Text);
+                e.Handled = true;
+            }
+        }
+
+        private void KeyHandler(object sender, KeyEventArgs e)
+        {
+            if (IsFocused)
+                switch (e.Key)
+                {
+                    case Key.Delete:
+                        SetWord(string.Empty);
+                        e.Handled = true;
+                        break;
+
+                    case Key.Right:
+                        if (FocusNext()) 
+                            e.Handled = true;
+                        break;
+                    case Key.Left:
+                        if (FocusPrevious())
+                            e.Handled = true;
+                        break;
+
+                    case Key.Back:
+                        if (string.IsNullOrEmpty(Word))
+                            FocusPrevious();
+                        else
+                            SetWord(Word.Substring(0, Word.Length - 1));
+                        e.Handled = true;
+                        break;
+
+                    case Key.Space:
+                    case Key.Tab:
+                        if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Shift))
+                            FocusPrevious();
+                        else
+                            FocusNext();
+                        e.Handled = true;
+                        break;
+
+                    default:
+                        // don't call it handled if we want to produce text for the text handler
+                        break;
+                }
+        }
+
+        private bool FocusNext()
+        {
+            if (this.NextInline == null)
+                return false;
+            this.NextInline.Focus();
+            return true;
+        }
+
+        private bool FocusPrevious()
+        {
+            if (this.PreviousInline == null)
+                return false;
+            this.PreviousInline.Focus();
+            return true;
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                return WordIsSet()
+                    && WordOrHasExactlyOneStop()
+                    && StopWordOrHasNoStops()
+                    && WordHasLengthLetters();
+            }
+        }
+
+        private bool WordIsSet()
+        {
+            return !string.IsNullOrEmpty(Word?.Trim());
+        }
+
+        private bool WordHasLengthLetters()
+        {
+            return LettersRegex.Matches(Word).Count == Length;
+        }
+
+        private bool StopWordOrHasNoStops()
+        {
+            return Length == StopDigitLength
+                || !StopsRegex.IsMatch(Word);
+        }
+
+        private bool WordOrHasExactlyOneStop()
+        {
+            return Length != StopDigitLength
+                || StopsRegex.Matches(Word).Count == 1;
+        }
+
+        public string Word { get; private set; }
+
+        internal void SetWord(string word)
+        {
+            Word = word;
+            if (string.IsNullOrEmpty(Word))
+                NoWordText();
+            else
+                WordText();
+
+            if (IsValid)
+                ValidFormat();
+            else
+                InvalidFormat();
+
+            SetBackground();
+        }
+
+        private void WordText()
+        {
+            Text = string.Format(
+                (Length != StopDigitLength
+                ? " {0}" 
+                : "{0}"),
+                Word, Length);
+        }
+
+        private void NoWordText()
+        {
+            Text = string.Format(
+                (Length != StopDigitLength
+                ? " {1}"
+                : ".{1}"), 
+                Word, Length);
+        }
+
+        private void InvalidFormat()
+        {
+            this.Foreground = System.Windows.Media.Brushes.Red;
+        }
+
+        private void ValidFormat()
+        {
+            this.Foreground = System.Windows.Media.Brushes.Black;
+        }
+    }
+}
